996e89a6559110651588143fd70f4a9acd36703f
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-01-27 16:48:30 -0500

    refactor: add factory method in Money and use in tests

--- money_spec.rb
+++ money_spec.rb
@@ -4,6 +4,9 @@ class Money
   def == money
     @amount == money.amount && self.class == money.class
   end
+  def self.dollar amount
+    Dollar.new(amount)
+  end
   protected
     attr_reader :amount
 
@@ -31,17 +34,17 @@ end
 
 RSpec.describe "Money" do
   it "can be multiplied" do
-    five = Dollar.new(5)
+    five = Money.dollar(5)
     expect(five.times(2)).to eq(Dollar.new 10)
     expect(five.times(3)).to eq(Dollar.new 15)
   end
 
   it "can be compared for equality" do
-    expect(Dollar.new(5)).to eq(Dollar.new(5))
-    expect(Dollar.new(5)).not_to eq(Dollar.new(6))
+    expect(Money.dollar(5)).to eq(Money.dollar(5))
+    expect(Money.dollar(5)).not_to eq(Money.dollar(6))
     expect(Franc.new(5)).to eq(Franc.new(5))
     expect(Franc.new(5)).not_to eq(Franc.new(6))
-    expect(Franc.new(5)).not_to eq(Dollar.new(5))
+    expect(Franc.new(5)).not_to eq(Money.dollar(5))
   end
 
   it "(Franc) can be multiplied" do
