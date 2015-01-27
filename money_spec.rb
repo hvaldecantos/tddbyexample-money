9832177d943b4d8653e1c32ff6072e18aca3c032
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-01-27 16:54:00 -0500

    refactor: add franc factory method in Money and use in tests

--- money_spec.rb
+++ money_spec.rb
@@ -7,6 +7,9 @@ class Money
   def self.dollar amount
     Dollar.new(amount)
   end
+  def self.franc amount
+    Franc.new(amount)
+  end
   protected
     attr_reader :amount
 
@@ -42,14 +45,14 @@ RSpec.describe "Money" do
   it "can be compared for equality" do
     expect(Money.dollar(5)).to eq(Money.dollar(5))
     expect(Money.dollar(5)).not_to eq(Money.dollar(6))
-    expect(Franc.new(5)).to eq(Franc.new(5))
-    expect(Franc.new(5)).not_to eq(Franc.new(6))
-    expect(Franc.new(5)).not_to eq(Money.dollar(5))
+    expect(Money.franc(5)).to eq(Money.franc(5))
+    expect(Money.franc(5)).not_to eq(Money.franc(6))
+    expect(Money.franc(5)).not_to eq(Money.dollar(5))
   end
 
   it "(Franc) can be multiplied" do
-    five = Franc.new(5)
-    expect(five.times(2)).to eq(Franc.new 10)
-    expect(five.times(3)).to eq(Franc.new 15)
+    five = Money.franc(5)
+    expect(five.times(2)).to eq(Money.franc 10)
+    expect(five.times(3)).to eq(Money.franc 15)
   end
 end
