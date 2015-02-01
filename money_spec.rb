b170421d6c3ad5db22bf3c02b8c452214e6232c3
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-02-01 14:59:13 -0500

    add equality Dollar#== method for comparing amount, and make attr_reader amount protected

--- money_spec.rb
+++ money_spec.rb
@@ -9,17 +9,24 @@ class Dollar
     Dollar.new @amount * multiplier
   end
 
-  def amount
-    @amount
+  def == dollar
+    @amount == dollar.amount
   end
+
+  protected
+    attr_reader :amount
+
 end
 
 RSpec.describe "Money" do
   it "can be multiplied" do
     five = Dollar.new(5)
-    product = five.times(2)
-    expect(product.amount).to eq(10)
-    product = five.times(3)
-    expect(product.amount).to eq(15)
+    expect(five.times(2)).to eq(Dollar.new 10)
+    expect(five.times(3)).to eq(Dollar.new 15)
+  end
+
+  it "can be compared for equality" do
+    expect(Dollar.new(5)).to eq(Dollar.new(5))
+    expect(Dollar.new(5)).not_to eq(Dollar.new(6))
   end
 end
