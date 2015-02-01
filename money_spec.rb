7b533a36aaf676ee7c8a763a50faccca68d4b216
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-02-01 15:09:08 -0500

    make Money#== based on amount and currency and get rid of Dollar and Franc classes

--- money_spec.rb
+++ money_spec.rb
@@ -6,31 +6,22 @@ class Money
     @currency = currency
   end
   def == money
-    @amount == money.amount && self.class == money.class
+    @amount == money.amount && self.currency == money.currency
   end
   def self.dollar amount
-    Dollar.new(amount, "USD")
+    Money.new(amount, "USD")
   end
   def self.franc amount
-    Franc.new(amount, "CHF")
+    Money.new(amount, "CHF")
+  end
+  def times multiplier
+    Money.new @amount * multiplier, @currency
   end
   def currency
     @currency
   end
   protected
     attr_reader :amount
-end
-
-class Franc < Money
-  def times multiplier
-    Franc.new @amount * multiplier, @currency
-  end
-end
-
-class Dollar < Money
-  def times multiplier
-    Dollar.new @amount * multiplier, @currency
-  end
 end
 
 RSpec.describe "Money" do
@@ -43,15 +34,7 @@ RSpec.describe "Money" do
   it "can be compared for equality" do
     expect(Money.dollar(5)).to eq(Money.dollar(5))
     expect(Money.dollar(5)).not_to eq(Money.dollar(6))
-    expect(Money.franc(5)).to eq(Money.franc(5))
-    expect(Money.franc(5)).not_to eq(Money.franc(6))
     expect(Money.franc(5)).not_to eq(Money.dollar(5))
-  end
-
-  it "(Franc) can be multiplied" do
-    five = Money.franc(5)
-    expect(five.times(2)).to eq(Money.franc 10)
-    expect(five.times(3)).to eq(Money.franc 15)
   end
 
   it "is possible to get the currency string" do
