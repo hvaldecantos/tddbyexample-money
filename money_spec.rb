d923882e2e8e6d5a6114dd0b0f940751e0de4489
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-02-01 15:07:17 -0500

    add currency parameter in Money's constructor to set currency attribute. Use subclass constructor in Dollar#times and Franc#times methods instead of Money's factory method

--- money_spec.rb
+++ money_spec.rb
@@ -1,45 +1,43 @@
 require 'rspec/core'
 
 class Money
+  def initialize amount, currency
+    @amount = amount
+    @currency = currency
+  end
   def == money
     @amount == money.amount && self.class == money.class
   end
   def self.dollar amount
-    Dollar.new(amount)
+    Dollar.new(amount, "USD")
   end
   def self.franc amount
-    Franc.new(amount)
+    Franc.new(amount, "CHF")
+  end
+  def currency
+    @currency
   end
   protected
     attr_reader :amount
-
 end
 
 class Franc < Money
-  def initialize amount
-    @amount = amount
-  end
-
   def times multiplier
-    Franc.new @amount * multiplier
+    Franc.new @amount * multiplier, @currency
   end
 end
 
 class Dollar < Money
-  def initialize amount
-    @amount = amount
-  end
-
   def times multiplier
-    Dollar.new @amount * multiplier
+    Dollar.new @amount * multiplier, @currency
   end
 end
 
 RSpec.describe "Money" do
   it "can be multiplied" do
     five = Money.dollar(5)
-    expect(five.times(2)).to eq(Dollar.new 10)
-    expect(five.times(3)).to eq(Dollar.new 15)
+    expect(five.times(2)).to eq(Money.dollar 10)
+    expect(five.times(3)).to eq(Money.dollar 15)
   end
 
   it "can be compared for equality" do
@@ -54,5 +52,10 @@ RSpec.describe "Money" do
     five = Money.franc(5)
     expect(five.times(2)).to eq(Money.franc 10)
     expect(five.times(3)).to eq(Money.franc 15)
+  end
+
+  it "is possible to get the currency string" do
+    expect("USD").to eq(Money.dollar(1).currency)
+    expect("CHF").to eq(Money.franc(1).currency)
   end
 end
